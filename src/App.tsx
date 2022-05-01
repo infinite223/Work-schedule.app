b817fe519793a6da6f58166162bdc8e6de1f5beb
infinite223 <dawidszmigiel9@gmail.com>
2022-05-01 13:24:36 +0200

    Start new projecct

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StartPage } from './StartPage/StartPage';
+
+function App() {
+  return (
+    <div>
+      <StartPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StartPage/StartPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './StartPage.scss'
+
+export const StartPage = () => {
+  return (
+    <div className='startpage flex'>
+        StartPage
+    </div>
+  )
+}
