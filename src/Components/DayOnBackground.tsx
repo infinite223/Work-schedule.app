cc8a2241d1817b1607517853477b7e495e6c83e7
infinite223 <dawidszmigiel9@gmail.com>
2022-05-14 00:54:06 +0200

    some animations and better view

--- src/Components/DayOnBackground.tsx
+++ src/Components/DayOnBackground.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState }from 'react'
+import { motion } from 'framer-motion';
 
 export const DayOnBackground:React.FC<{ className:string, day:number }> = ({className, day}) => {
+  const [ animatedClass, setAnimatedClass ] = useState("")
+  const d = new Date();
+  useEffect(() => {
+    if(d.getDate()===day){
+      setAnimatedClass("thisDay")
+    }
+  },[])
+
   return (
-    <div className={className}><text>{day}<span>May</span></text></div>
+    <motion.div className={animatedClass+" "+className} ><text>{day}<span>May</span></text></motion.div>
   )
 }
