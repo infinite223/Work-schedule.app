98a9da206be96949e1d0fa7a7323ee59314292ad
infinite223 <dawidszmigiel9@gmail.com>
2022-05-17 13:28:34 +0200

    starting crate view login page

--- src/LoginPage/LoginPage.tsx
+++ src/LoginPage/LoginPage.tsx
@@ -1,12 +1,17 @@
 import { useNavigate, HiOutlineChevronDoubleLeft, motion } from './../imports';
 import { show } from './../Animations/variants';
+
+import './LoginPage.scss'
+
 export const LoginPage = () => {
-    const navigate = useNavigate(); 
+  const navigate = useNavigate(); 
   return (
     <div>
          <HiOutlineChevronDoubleLeft className='icon-exit'  onClick={()=>navigate("/")}/>
          <motion.div variants={show} initial="hidden" animate="visible" className='LoginPage flex'>
-           LoginPage
+           <h1>Log In</h1>
+           <button className='login button'>Log In</button>
+
          </motion.div>
     </div>
   )
