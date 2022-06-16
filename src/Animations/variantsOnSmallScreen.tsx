790a61d0f86d3eb496c526d1223fa74983cb5c2b
infinite223 <dawidszmigiel9@gmail.com>
2022-06-16 15:28:28 +0200

    better look create page

---
+++ src/Animations/variantsOnSmallScreen.tsx
@@ -0,0 +1,10 @@
+export const showMobilePage = {
+    hidden: {opacity: 0, scale:1, y:400},
+    visible: {
+      opacity: 1, scale:1, y:0,
+      transition: {
+        delay:.2,
+        duration:.5,
+      }
+    }  
+}
--- src/Components/CreateSheudle/CreateSchedule.tsx
+++ src/Components/CreateSheudle/CreateSchedule.tsx
@@ -1,10 +1,10 @@
 import { motion, HiOutlineChevronDoubleLeft, useNavigate, Admin, Worker, useDispatch, bindActionCreators } from './../../imports'
 import { useSelector } from 'react-redux';
 import { BsPersonBoundingBox } from 'react-icons/bs'
-import { IoCreateOutline } from 'react-icons/io5'
 import { actionCreators,  State } from '../../state';
 
 import { position, showPage } from './../../Animations/variants';
+import { showMobilePage } from '../../Animations/variantsOnSmallScreen';
 import { MdOutlinePersonAdd } from 'react-icons/md'
 
 import './CreateSchedule.scss'
@@ -19,53 +19,55 @@ export const CreateSchedule = () => {
   const { addPerson, setPerson, deletePerson } = bindActionCreators(actionCreators, dispatch)
 
   return (
-    <motion.div className='CreateSchedule' variants={showPage} initial="hidden" animate="visible">
+    <>
       <HiOutlineChevronDoubleLeft className='icon-exit'  onClick={()=>navigate("/")}/>
-      <nav>
-        <motion.div
-          initial={{opacity:0}}
-          animate={{opacity:1}}
-          transition={{duration:2.5}}
-          > 
-            <h3>Persons in schedule <text style={{color:"#FF00FF"}}>{person.length}</text></h3>
+      <div className='right-header'>Create</div>
 
-          </motion.div>   
-          <div className='create__group'>
-              <text className='text__value'>
-                  {/* <text>Name group:</text> */}
-                <input type="text" placeholder='Name group'/> 
-                {/* <IoCreateOutline size={25} className="create__group-button"/> */}
-              </text> 
-            </div>  
-      </nav>
-      <div className='Content flex'>
-        <div className='Persons'>               
-          {person.map((worker:any)=> {
-            return <Worker lastPersonId={person.at(-1).id} key={worker.id} id={worker.id}/>
-          })}
-         
-            <motion.div className='Person' 
-              variants={position}
-              initial="outsideTop"
-              animate="goodPosition"
-              whileHover="hoverWorker">
-              <motion.div className="Person__add-button flex" onClick={()=> (addPerson({id: persons.length,email:"", nickname:""}))}>
-                <h5 style={{marginLeft:"20px"}}>Click to add new person </h5> 
-                <MdOutlinePersonAdd size={25} color='white' style={{margin:"0 20px"}}/>
+      <motion.div className='CreateSchedule' variants={showMobilePage} initial="hidden" animate="visible">
+        <nav>
+          <motion.div
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:2.5}}
+            > 
+              <h3>Persons in schedule <text style={{color:"#FF00FF"}}>{person.length}</text></h3>
+
+            </motion.div>   
+            <div className='create__group'>
+                <text className='text__value'>
+                  <input type="text" placeholder='Set name group...'/> 
+                </text> 
+              </div>  
+        </nav>
+        <div className='Content flex'>
+          <div className='Persons'>               
+            {person.map((worker:any)=> {
+              return <Worker lastPersonId={person.at(-1).id} key={worker.id} id={worker.id}/>
+            })}
+          
+              <motion.div className='Person' 
+                variants={position}
+                initial="outsideTop"
+                animate="goodPosition"
+                whileHover="hoverWorker">
+                <motion.div className="Person__add-button flex" onClick={()=> (addPerson({id: persons.length,email:"", nickname:""}))}>
+                  <h5 style={{marginLeft:"20px"}}>Click to add new person </h5> 
+                  <MdOutlinePersonAdd size={25} color='white' style={{margin:"0 20px"}}/>
+                </motion.div>
               </motion.div>
-            </motion.div>
-     
-        </div> 
-        <div className='Person__data flex'>
-            <div className='Person__data-admin'>
-              <text>Admin<div style={{color:"#FF00FF", fontSize:"13px"}}>(YOU)</div></text>
+      
+          </div> 
+          <div className='Person__data flex'>
+              <div className='Person__data-admin'>
+                <text>Admin<div style={{color:"#FF00FF", fontSize:"13px"}}>(YOU)</div></text>
 
-               {/* <Admin/> */}
-            </div>
-              
-            <BsPersonBoundingBox size={100} className='Person__data-icon'/>
-        </div> 
-      </div>
-    </motion.div>
+                {/* <Admin/> */}
+              </div>
+                
+              <BsPersonBoundingBox size={100} className='Person__data-icon'/>
+          </div> 
+        </div>
+      </motion.div>
+    </>
   )
 }
