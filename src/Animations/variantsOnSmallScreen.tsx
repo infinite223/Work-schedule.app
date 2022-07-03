3530c75704dcaf3b37248066a63c2ce4191c31a5
infinite223 <dawidszmigiel9@gmail.com>
2022-07-03 14:48:36 +0200

    some changes

--- src/Animations/variantsOnSmallScreen.tsx
+++ src/Animations/variantsOnSmallScreen.tsx
@@ -1,10 +1,10 @@
 export const showMobilePage = {
-    hidden: {opacity: 0, scale:1, y:400},
+    hidden: {opacity: 0, scale:1, y:-400},
     visible: {
       opacity: 1, scale:1, y:0,
       transition: {
         delay:.2,
-        duration:.5,
+        duration:1,
       }
     }  
 }
