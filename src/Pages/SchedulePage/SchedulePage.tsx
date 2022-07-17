ef16c1c9b3eab5024f8cb14b70407ca1e5e45504
infinite223 <dawidszmigiel9@gmail.com>
2022-07-17 13:50:50 +0200

    fixed bug

--- src/Pages/SchedulePage/SchedulePage.tsx
+++ src/Pages/SchedulePage/SchedulePage.tsx
@@ -54,6 +54,7 @@ export const SchedulePage = () => {
     const navigate = useNavigate()
     const controlDay = useAnimation()
     const controlSchedule = useAnimation()
+    const controlArrow = useAnimation()
 
     const {setPersonInDay, setSchedule} = bindActionCreators(actionCreators, dispatch)
 
@@ -66,13 +67,19 @@ export const SchedulePage = () => {
 
       useEffect(()=>{
         isTabletOrMobile&&(
-        controlSchedule.start({
-          height:!schowSchedule?"50px":"auto",
-          overflow:!schowSchedule?"hidden":"",
-          transition: { duration: 1, overflow: schowSchedule?{
-            delay:1
-          }:{delay:0} },          
-        }))
+            controlSchedule.start({
+            height:!schowSchedule?"0px":"auto",
+            y:!schowSchedule?-500:0,
+            overflow:!schowSchedule?"hidden":"",
+            transition: { duration: 1, overflow: schowSchedule?{
+                delay:1
+            }:{delay:0} },          
+            })
+        )
+        controlArrow.start({
+            opacity:[0,1],
+            transition: {duration:2}         
+        })    
       }, [schowSchedule])
 
     useEffect(()=>{  
@@ -143,46 +150,52 @@ export const SchedulePage = () => {
                 <span>{loginPerson}</span>
                 <GiHamburgerMenu className='hover__pointer' size={18} style={{marginLeft:"15px"}} onClick={()=>setShowMenu(true)}/>
             </div>:
-            !showMenu&&<GiHamburgerMenu size={24} onClick={()=>setShowMenu(true)} className="menu"/>
+           <></>
         }
         {showMenu&&<MenuModal showSettings={showSettings} setShowSettings={setShowSettings} showMenu={showMenu} setShowMenu={setShowMenu} updateSchedule={()=>updateSchedule()}/>}
         {showSettings&&<SettingsModal theme={theme} setTheme={setTheme} setShowSettings={setShowSettings}/>}
         {(showMenu || showSettings)&& <div className='blur-page' onClick={()=>(setShowMenu(false),setShowSettings(false))}/>}
-        <motion.div className='SchedulePage'  variants={isTabletOrMobile?showMobilePage:showPage} initial="hidden" animate="visible">
+        <motion.div className='SchedulePage'  variants={isTabletOrMobile?showMobilePage:showPage} initial="hidden" animate="visible">         
             <motion.div
              style={isTabletOrMobile?{  background: "linear-gradient(0deg, rgba("+theme+", .5) 66%, rgba("+theme+",.7) 85%, rgba("+theme+", 0.9) 96%)",
              boxShadow: "inset 0px -5px 0px 0px rgb("+theme+")"}:{}}
-             className='SchedulePage__main'  initial="hidden" animate={controlSchedule}>
-                <div className='SchedulePage__navbar flex'>
-                    <div className='date'>
-                        <div className='year'>{selectDate.getFullYear()}</div> 
-                        <nav className='flex'>
-                            <div className='arrow-left' onClick={()=>setSelectDate(new Date(selectDate.getFullYear(), selectDate.getMonth()-1,1))}><MdOutlineArrowBackIosNew size={20}/></div>
-                            <div className='month'>{month[selectDate.getMonth()]}</div>                     
-                            <div className='arrow-right' onClick={()=>setSelectDate(new Date(selectDate.getFullYear(), selectDate.getMonth()+1, 1))}><MdOutlineArrowBackIosNew size={20}/></div>
-                        </nav>
-                        {!isTabletOrMobile&&<WorkerList/>}
-                    </div>
-                
-                </div>
-                <motion.div animate={controlDay} >
-                    <motion.div key="box" animate={controlSchedule}  className='SchedulePage__content flex'>
-                        {daysShortcuts.map((day)=>{return <div key={day} className='daysOfTheWeek'>
-                            {day.substring(0,2)}
-                        </div>})}
-
-                    {firstDayOfMonth(selectDate).map((i)=>{
-                        return  <div key={i} className='empty-day'></div>
-                    })}
+             className='SchedulePage__main'  initial="hidden">
+                <nav>
+                    <div className='year'>{selectDate.getFullYear()}</div>                  
+                     {isTabletOrMobile&&<motion.div animate={controlArrow}>{!schowSchedule&&<MdKeyboardArrowDown size={30} className="arrow-icon-top" onClick={()=>setShowSchedule(true)}/>}</motion.div>}
+                    {!showMenu&&<GiHamburgerMenu size={24} onClick={()=>setShowMenu(true)} className="menu"/>}
+                 </nav>
+                <motion.div className='flex schedule__content-all' animate={controlSchedule}>
+                    <div className='SchedulePage__navbar flex'>
+                        <div className='date'>                           
+                            <nav className='flex'>
+                                <div className='arrow-left' onClick={()=>setSelectDate(new Date(selectDate.getFullYear(), selectDate.getMonth()-1,1))}><MdOutlineArrowBackIosNew size={20}/></div>
+                                <div className='month'>{month[selectDate.getMonth()]}</div>                     
+                                <div className='arrow-right' onClick={()=>setSelectDate(new Date(selectDate.getFullYear(), selectDate.getMonth()+1, 1))}><MdOutlineArrowBackIosNew size={20}/></div>
+                            </nav>
+                            {!isTabletOrMobile&&<WorkerList/>}
+                        </div>
                     
-                        {schedule.map((day)=>{
-                            return (
-                                <Day key={day.id} id={day.id}  persons={day.persons}/>
-                            )
+                    </div>
+                    <motion.div animate={controlDay} >
+                        <motion.div key="box" animate={controlSchedule}  className='SchedulePage__content flex'>
+                            {daysShortcuts.map((day)=>{return <div key={day} className='daysOfTheWeek'>
+                                {day.substring(0,2)}
+                            </div>})}
+
+                        {firstDayOfMonth(selectDate).map((i)=>{
+                            return  <div key={i} className='empty-day'></div>
                         })}
+                        
+                            {schedule.map((day)=>{
+                                return (
+                                    <Day key={day.id} id={day.id}  persons={day.persons}/>
+                                )
+                            })}
+                        </motion.div>
                     </motion.div>
+                    {isTabletOrMobile&&<motion.div animate={controlArrow}>{showSchedule&&<MdKeyboardArrowDown size={30} className="arrow-icon" onClick={()=>setShowSchedule(false)}/>}</motion.div>}
                 </motion.div>
-                {isTabletOrMobile&&<MdKeyboardArrowDown size={30} className={schowSchedule?"arrow-icon":"arrow-icon-top"} onClick={()=>setShowSchedule(!schowSchedule)}/>}
             </motion.div>
             {isTabletOrMobile&&<>
                 <div style={{overflowY:"scroll"}}>
