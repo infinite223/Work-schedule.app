8048e470a869bb0654a9740afcc238de0e8a9ce3
infinite223 <dawidszmigiel9@gmail.com>
2022-07-19 22:14:57 +0200

    new changes

--- src/Components/CreateSheudle/CreateSchedule.tsx
+++ src/Components/CreateSheudle/CreateSchedule.tsx
@@ -8,18 +8,20 @@ import { showMobilePage } from '../../Animations/variantsOnSmallScreen';
 import { showPage } from './../../Animations/variants';
 import { useMediaQuery } from 'react-responsive'
 import { db } from '../../firebase';
-import { setDoc, getDoc, doc } from 'firebase/firestore';
+import { setDoc, getDoc, getDocs, collection,  doc } from 'firebase/firestore';
 import { generateSheduleData, daysInMonth } from '../../Helpers/functions/functions';
 import { MessageModal } from '../MessageModal';
 import { today, month } from '../../Helpers/constants';
 
 import './CreateSchedule.scss'
+import { workerBeforSign } from '../../Helpers/types';
 
 export const CreateSchedule = () => {
   const navigate = useNavigate(); 
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' })
   const [ nameGroup, setNameGroup ] = useState("")
   const persons = useSelector((state: State)=> state.person)
+  const [workers, setWorkers] = useState<Array<{id:number, email:string, nickname:string}>>([{id:1, email:"", nickname:""}])
   const  person :any = persons;
   const dispatch = useDispatch();
 
@@ -29,44 +31,21 @@ export const CreateSchedule = () => {
   const { addPerson } = bindActionCreators(actionCreators, dispatch)
   const [showMessage, setShowMessage] = useState(false)
   const [message, setMessage] = useState({descripstion:"", status:false})
+  const [validateWorkers, setValidateWorkers] = useState(false)
 
   const createSchedule = async () => {
-    // if(persons.length<=1){
-    //   if(nameGroup.length>=4){
-    //     if(emailAdmin.length > 3 || nicknameAdmin.length > 3){
-    //       const foundGroup = await getDoc(doc(db, "groups", nameGroup))  
-    //       if(!foundGroup.data()){
-    //         await setDoc(doc(db, "groups", nameGroup), {
-    //           nameGroup:nameGroup,
-    //           workers: person,
-    //           admin: {
-    //             nickname:nicknameAdmin,
-    //             email:emailAdmin
-    //           }
-    //         }).then( async ()=> {
-    //           await setDoc(doc(db, "schedule", nameGroup), {
-    //             [month[today.getMonth()] + today.getFullYear()]: generateSheduleData(daysInMonth(new Date()))
-    //           }).then(() => (setMessage({descripstion:"The group has been created correctly", status:true}), setShowMessage(true)))
-    //         })
-    //       }
-    //       else {
-    //         setMessage({descripstion:"Group name taken, use another", status:false}); setShowMessage(true)
-    //       }
-    //     }
-    //     else {
-    //       setMessage({descripstion:"Email or nickname is too short", status:false}); setShowMessage(true)
-    //     }
-    //   }
-    //   else {
-    //     setMessage({descripstion:"The group name must be at least 4 letters long", status:false}); setShowMessage(true)
-    //   }
-    // }
-    // else {
-    //   setMessage({descripstion:"Without employees, you cannot create a schedule", status:false}); setShowMessage(true);
-    // }
-
     const foundGroup = await getDoc(doc(db, "groups", nameGroup))  
-    if(persons.length<1){
+    const groups = await getDocs(collection(db, "groups"))
+    groups.docs.forEach((doc)=>{
+      if(doc.data().workers.find((worker:workerBeforSign)=>worker.email===emailAdmin)){
+        console.log("zajęty email!")
+      }
+    })
+
+    if(validateWorkers){
+      setMessage({descripstion:"Email or nickname is too short", status:false}); setShowMessage(true)
+    }
+    else if(persons.length<1){
       setMessage({descripstion:"Without employees, you cannot create a schedule", status:false}); setShowMessage(true);
     }
     else if(nameGroup.length<4){
@@ -100,7 +79,7 @@ export const CreateSchedule = () => {
       <HiOutlineChevronDoubleLeft className='icon-exit'  onClick={()=>navigate("/")}/>
       <motion.div initial={!isTabletOrMobile?{right:"-85px"}:{right:"0px"}} whileHover={{right:"0px"}} transition={{duration:.5}} className='right-header flex'>
             {!isTabletOrMobile&&<HiOutlineChevronDoubleLeft size={30} className='icon-save'/>}
-            <span onClick={()=>createSchedule()}>Create</span>
+            <span onClick={()=>(createSchedule())}>Create</span>
             {isTabletOrMobile&&<HiOutlineChevronDoubleLeft size={30} className='icon-save'/>}
       </motion.div>
 
@@ -112,18 +91,17 @@ export const CreateSchedule = () => {
             transition={{duration:2.5}}
             > 
               <h3>Persons in schedule <span style={{color:"#FF00FF"}}>{person.length}</span></h3>
-
             </motion.div>   
             <div className='create__group'>
                 <span className='text__value'>
                   <input type="text" placeholder='Set name group...' onChange={(x)=>setNameGroup(x.target.value)}/> 
                 </span> 
-              </div>  
-        </nav>
+            </div>  
+        </nav>  
         <div className='Content flex'>
           <div className='Persons'>               
-            {person.map((worker:any)=> {
-              return <Worker key={worker.id} id={worker.id} setMessage={setMessage}/>
+            {persons.map((worker:any)=> {
+              return <Worker key={worker.id} createSchedule={()=>createSchedule()} id={worker.id} validateWorkers={validateWorkers} setWorkers={setWorkers} setMessage={setMessage}/>
             })}
           
               <motion.div className='Person' 
--- src/Components/CreateSheudle/Persons/Worker.tsx
+++ src/Components/CreateSheudle/Persons/Worker.tsx
@@ -5,9 +5,16 @@ import { actionCreators, State } from '../../../state';
 
 import './Person.scss'
 
+interface workerProps {
+  id:number,
+  setMessage:(value:{descripstion:string,
+  status:boolean}) => void,
+  setWorkers:(value:Array<{id:number, email:string, nickname:string}>)=> void,
+  validateWorkers:boolean,
+  createSchedule:()=>void
+}
 
-
-export const Worker: React.FC<{ id:number, setMessage:(value:{descripstion:string, status:boolean}) => void }>= ({ id, setMessage }) => { 
+export const Worker: React.FC<workerProps>= ({ id, setMessage, validateWorkers, createSchedule }) => { 
   const person = useSelector((state: State)=> state.person)
   const dispatch = useDispatch();
   
@@ -15,16 +22,20 @@ export const Worker: React.FC<{ id:number, setMessage:(value:{descripstion:strin
 
   const [email, setEmaill] = useState(person[id]?person[id].email:"")
   const [nickname, setNickname] = useState<string>(person[id]?person[id].nickname:"")
+  console.log(email)
 
   useEffect(()=>{
-    if(email.length<3 || nickname.length<3){
-      //setMessage({descripstion:"Email or nickname is too short", status:false})
-    }
-    else {
-      setMessage({descripstion:"", status:true})
-    }
+    //if(validateWorkers){
+      setPerson({id:id,email:email,nickname:nickname})
+      if(email.length<3 || nickname.length<3){
+        
+      }
+      else {
+        setMessage({descripstion:"", status:true})
+      }
+    //}
   },[email, nickname])
-
+  
   return (
     <div className='Person'>
           <motion.div className='person_content'  variants={show} initial="hidden" animate="visible"  whileHover="hover">
@@ -32,10 +43,10 @@ export const Worker: React.FC<{ id:number, setMessage:(value:{descripstion:strin
               
               <div className='person__data'>
                 <div className='div__value'><div className='label'>Email:</div>
-                  <input type="div" autoComplete='off' placeholder='Email' value={email} onChange={(x) => (setEmaill(x.target.value), setPerson({id:id,email:email,nickname:nickname}))}/>
+                  <input type="div" autoComplete='off' placeholder='Email' value={email} onChange={ (x) => setEmaill(x.target.value)}/>
                 </div>
                 <div className='div__value'><div className='label'>Nickname:</div>
-                  <input type="div" placeholder='Nickname' value={nickname} onChange={(x)=>(setNickname(x.target.value),setPerson({id:id, email:email, nickname:nickname}))}/>
+                  <input type="div" placeholder='Nickname' value={nickname} onChange={(x)=> setNickname(x.target.value)}/>
                 </div>    
               </div>            
               <MdOutlinePersonRemove className='delete-person' size={25} onClick={()=>deletePerson({id:id,email:"",nickname:""})}/>
